a85322cfa1cea6a4b215af211a9347de9fedc154
Yassin Chamlal <6002714@mborijnland.nl>
2021-06-09 13:48:19 +0200

    Footer aangepast
    bij tablet staan de bloken naast elkaar en bij mobile is de handige links block weg

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -8,100 +8,100 @@
     <title>Imkerwebsite-Home</title>
 </head>
 <body>
-<div class="container-fluid mx-auto">
 
-    <!-- Footer -->
-    <footer class="text-center text-lg-start text-dark bg-warning"  >
 
-        <!-- Section: Links  -->
-        <section class="">
-            <div class="container text-center text-md-start mt-5 mb-3">
-                <!-- Grid row -->
-                <div class="row mt-3">
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 mt-3">
-                        <!-- Content -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Imkerverening
-                        </h6>
-                        <p>
-                            De imkervereniging stelt zich ten doel de kennis over het houden van bijen en de relatie van bijen met hun (planten) omgeving te verbreden bij zowel de imkers, als bij het grotere publiek.
-                        </p>
-                    </div>
-                    <!-- Grid column -->
+<!-- Footer -->
+<footer class="text-center text-lg-start text-dark bg-warning"  >
 
-                    <!-- Grid column -->
-                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 mt-3">
-                        <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Sociaal
-                        </h6>
-                        <p>
-                            <a href="#!" class="text-reset">Instagram</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Twitter</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Facebook</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Nieuws berichten</a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 mt-3">
-                        <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Handige links
-                        </h6>
-                        <p>
-                            <a href="#!" class="text-reset">Home</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Berichten</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Informatie</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Contact</a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 mt-3">
-                        <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Contact
-                        </h6>
-                        <p>  <i class="bi bi-house-door"></i>   Poelgeesterweg 1, Oegstgeest.</p>
-                        <p>
-                            <i class="bi bi-envelope"></i>
-                            secretaris@imkervereniging-oegstgeest.nl
-                        </p>
-
-                    </div>
-                    <!-- Grid column -->
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5 mb-3  ">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-sm-5 col-xl-3   mx-auto mb-4 mt-3 ">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4 ">
+                        Imkerverening
+                    </h6>
+                    <p>
+                        De imkervereniging stelt zich ten doel de kennis over het houden van bijen en de relatie van bijen met hun (planten) omgeving te verbreden bij zowel de imkers, als bij het grotere publiek.
+                    </p>
                 </div>
-                <!-- Grid row -->
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2  col-sm-5 mx-auto mb-4 mt-3 col-xs-6">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Sociaal
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Instagram</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Twitter</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Facebook</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Nieuws berichten</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2  col-sm-5 mx-auto mb-4 mt-3 d-none d-sm-block">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Handige links
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Home</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Berichten</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Informatie</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Contact</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 col-sm-5  mx-auto mb-md-0 mb-4 mt-3">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Contact
+                    </h6>
+                    <p>  <i class="bi bi-house-door"></i>   Poelgeesterweg 1, Oegstgeest.</p>
+                    <p>
+                        <i class="bi bi-envelope"></i>
+                        secretaris@imkervereniging-oegstgeest.nl
+                    </p>
+
+                </div>
+                <!-- Grid column -->
             </div>
-        </section>
-        <!-- Section: Links  -->
-
-        <!-- Copyright -->
-        <div class="text-center p-4 bg-warning" >
-            © 2021 Copyright:
-            <a class="text-reset fw-bold" href=" https://www.imkervereniging-oegstgeest.nl/">Imkervereniging Oegstgeest  </a>
+            <!-- Grid row -->
         </div>
-        <!-- Copyright -->
-    </footer>
-    <!-- Footer -->
+    </section>
+    <!-- Section: Links  -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <!-- Copyright -->
+    <div class="text-center p-4 bg-warning" >
+        © 2021 Copyright:
+        <a class="text-reset fw-bold" href=" https://www.imkervereniging-oegstgeest.nl/">Imkervereniging Oegstgeest  </a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
 
 </body>
